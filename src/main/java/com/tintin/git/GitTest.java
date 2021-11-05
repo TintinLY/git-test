08b60ec8a92f6993ab42edab568a894bfa1705ca
Tintin <821294434@qq.com>
2021-11-05 21:08:20 +0800

    V1

---
+++ src/main/java/com/tintin/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.tintin.git;
+
+/**
+ * @author tintin
+ * @create 2021-11-05-21:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git-test");
+        return;
+    }
+}
